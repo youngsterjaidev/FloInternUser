16c64cd8c709627c2d79af9e6770129270d5b34b
INFNA <jaidevv999@outlook.com>
2021-07-03 16:56:52 +0530

    Add the base files

---
+++ main.ts
@@ -0,0 +1,15 @@
+//== Main.ts ==//
+
+/**
+ * Main function will return nothing contains all the function executed
+ */
+
+const main = (): void => {
+    console.log("Server is on fire !")
+
+    // get all the users list
+    return
+}
+
+// call the main function
+main()
